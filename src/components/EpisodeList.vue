436315d1113d6e47347192af531938adda955cea
Jouan <jouan.lin@outlook.com>
2025-02-26 15:52:58 +0800

    fix: Format the publish date

--- src/components/EpisodeList.vue
+++ src/components/EpisodeList.vue
@@ -7,8 +7,14 @@ const router = useRouter();
 
 const goToEpisode = (id: string) => {
 	store.setSelectedEpisodeIndex(id);
-    router.push(`/episode/${id}`);
+	router.push(`/episode/${id}`);
 };
+
+const formatTime = (dateString: string) => {
+	const date = new Date(dateString);
+	const formattedDate = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
+	return formattedDate
+}
 
 </script>
 
@@ -21,22 +27,21 @@ const goToEpisode = (id: string) => {
 			</tr>
 		</thead>
 		<tbody>
-			<tr v-for="episode in store.channel.episodes" :key="episode.guid" class="channel-item relative h-[60px] cursor-default text-[15px] hover:bg-gray-700">
+			<tr v-for="episode in store.channel.episodes" :key="episode.guid"
+				class="channel-item relative h-[60px] cursor-default text-[15px] hover:bg-gray-700">
 				<td class="h-[60px] w-[86px] text-center overflow-hidden px-2 py-4">
 					<img :src="store.channel.imageUrl" alt="Episode" class="h-auto rounded-lg" />
 				</td>
 				<td class="h-[60px] overflow-hidden px-8 py-4 text-start">
 					<div>
-						<h2 
-							class="w-fit text-xl font-semibold mt-2 hover:underline hover:text-gray-300 cursor-pointer"
-							@click="goToEpisode(episode.guid)"
-						>
+						<h2 class="episode-item w-fit text-xl font-semibold mt-2 hover:underline hover:text-gray-300 cursor-pointer"
+							@click="goToEpisode(episode.guid)">
 							{{ episode.title }}
 						</h2>
-						<p class="text-[#757575]">{{ episode.publishDate }}</p>
+						<p class="text-[#757575]">{{ formatTime(episode.publishDate) }}</p>
 					</div>
 				</td>
-				
+
 			</tr>
 		</tbody>
 	</table>
